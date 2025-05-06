c14622d91d4ad5219c78c32e5d61b8a837657a3c
Eetu <148777334+eetmie@users.noreply.github.com>
2025-05-06 16:09:02 +0300

    clean up project
    Nothing huge, removed old files and general cleaning. Stay tuned!

--- IsaacLab/source/isaaclab_assets/isaaclab_assets/robots/masiV0.py
+++ IsaacLab/source/isaaclab_assets/isaaclab_assets/robots/masiV0.py
@@ -1,131 +1,176 @@
-"""Test Configuration for the MASI excavator robot."""
+"""Configuration for the MASI excavator robot."""
 
 from __future__ import annotations
 
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.assets import ArticulationCfg
-from isaaclab.sensors import ImuCfg
+
+
 
 ##
 # Configuration
 ##
 
+# Model0, lift, tilt, scoop
 MASIV0_CFG = ArticulationCfg(
+    # alot of example configs, but not all are needed
     prim_path="{ENV_REGEX_NS}/Robot",
     spawn=sim_utils.UsdFileCfg(
-
         # Hardcoded path to the .usd file!
         usd_path="C:/Users/sh23937/Documents/GitHub/IsaacSim/IsaacSim Models/Test_Bench_Model/for_scripts/model0.usd",
 
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            # Required properties from other examples
-            disable_gravity=False,          # Enable/disable gravity
-            retain_accelerations=False,     # Whether to keep acceleration data
-            linear_damping=0.0,             # Linear motion damping coefficient
-            angular_damping=0.0,            # Angular motion damping coefficient
-            max_linear_velocity=1000.0,     # Maximum linear velocity limit
-            max_angular_velocity=1000.0,    # Maximum angular velocity limit
-            max_depenetration_velocity=1.0, # Maximum velocity for collision resolution
+            # Required properties
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=5.0,  # Increased from 1.0 to help with collision resolution (like Franka)
 
-            # Optional properties from other examples
-            enable_gyroscopic_forces=True,  # Enable gyroscopic forces
-            # max_contact_impulse=1e32,               # Maximum contact force allowed
-            # enable_momentum_forces=False,           # Enable momentum preservation
-            # disable_preprocessing=False,            # Disable constraint preprocessing
-            # disable_ccd=False,                      # Disable continuous collision detection (not tested)
+            # Additional properties from examples
+            enable_gyroscopic_forces=True,
+            max_contact_impulse=1e4,  # Maximum contact force allowed (limit impacts)
         ),
+
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,  # Keep false for efficiency if no self-collisions expected
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
 
-            enabled_self_collisions=False,      # Whether to enable self-collisions
-            solver_position_iteration_count=4,  # Number of position iterations for physics solver
-            solver_velocity_iteration_count=1,  # Number of velocity iterations for physics solver
-            sleep_threshold=0.005,              # Threshold for putting articulation to sleep
-            stabilization_threshold=0.001,      # Threshold for stabilization
-
-            # enable_self_collisions=False,           # Global self-collision setting
-            # self_collision_filter_pairs=[],         # Specify collision filtering pairs
-            # enable_driving_constraints=True,        # Enable joint constraints
-            # enable_projection=True,                 # Enable joint projection
-            # projection_iterations=4,                # Number of projection iterations
-            # min_position_iteration_count=2,         # Minimum position solver iterations
-            # min_velocity_iteration_count=1,         # Minimum velocity solver iterations
         ),
-        copy_from_source=True,                        # Copy configs from source USD file (if available, setting values here will override)
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.0),                   # Z1.0 up from ground for debugging. EDIT: dont change here, change in the scene!
-        # rot=(1.0, 0.0, 0.0, 0.0),            # Optional: Initial rotation (w,x,y,z)
-        # lin_vel=(0.0, 0.0, 0.0),             # Optional: Initial linear velocity
-        # ang_vel=(0.0, 0.0, 0.0),             # Optional: Initial angular velocity
 
-        # testing (env resets wrong)
-        #joint_pos={
-        #    "revolute_lift": 0.0,               # radians!
-        #    "revolute_tilt": 0.0,
-        #    "revolute_scoop": 0.0,
-        #},
-        # joint_vel={},                         # Optional: Initial joint velocities
-        # joint_effort={},                      # Optional: Initial joint efforts
+
+        collision_props=sim_utils.CollisionPropertiesCfg(
+            contact_offset=0.002,  # Distance to start collision detection
+            rest_offset=0.0,  # Rest distance between colliding objects
+        ),
+
+
+        joint_drive_props=sim_utils.JointDrivePropertiesCfg(
+            drive_type="position",  # Can be "force" or "position"
+        ),
+
+        copy_from_source=True,
     ),
+
+    init_state=ArticulationCfg.InitialStateCfg(
+        #pos=(0.0, 0.0, 0.0),
+
+        # initial joint angles in radians
+        joint_pos={
+            "revolute_lift": 0.0,
+            "revolute_tilt": 0.0,
+            "revolute_scoop": 0.0,
+        },
+        joint_vel={".*": 0.0},  # Set all joint velocities to zero initially
+    ),
+
+    # Soft joint limits to prevent hitting hard stops
+    soft_joint_pos_limit_factor=1.00,
+
     actuators={
         "excavator": ImplicitActuatorCfg(
             joint_names_expr=["revolute_lift", "revolute_tilt", "revolute_scoop"],
+            stiffness={
+                "revolute_lift": 1000,
+                "revolute_tilt": 1000,
+                "revolute_scoop": 1000,
+            },
+            damping={
+                "revolute_lift": 1.5,
+                "revolute_tilt": 1.5,
+                "revolute_scoop": 1.5,
+            },
+            velocity_limit={
+                "revolute_lift": 0.6,
+                "revolute_tilt": 0.6,
+                "revolute_scoop": 0.6,
+            },
 
-
-            # custom actuator properties
-             stiffness={
-                 "revolute_lift": 1000,
-                 "revolute_tilt": 1000,
-                 "revolute_scoop": 1000,
-             },
-             damping={
-                 "revolute_lift": 1.5,
-                 "revolute_tilt": 1.5,
-                 "revolute_scoop": 1.5,
-             },
-            # inf effort to reduce bouncing
-             #effort_limit={
-             #    "revolute_lift": 5000.0,
-             #    "revolute_tilt": 5000.0,
-             #    "revolute_scoop": 5000.0,
-             #},
-             velocity_limit={
-                 "revolute_lift": 0.6,
-                 "revolute_tilt": 0.6,
-                 "revolute_scoop": 0.6,
-             },
+            friction={
+                "revolute_lift": 0.1,
+                "revolute_tilt": 0.1,
+                "revolute_scoop": 0.1,
+            },
+            # armature = "shaft inertia"
+            armature={
+                "revolute_lift": 0.01,
+                "revolute_tilt": 0.01,
+                "revolute_scoop": 0.01,
+            },
         ),
     },
 )
 
 
-# sensors, now removed for V0 (using direct transforms) but kept for reference
-"""
-V0_IMU_CFG = {
-    "imu_lift": ImuCfg(
-        prim_path="{ENV_REGEX_NS}/Robot/imu_liftboom",
-        update_period=0.0,
-        debug_vis=True, # Visualize the sensor (red arrow)
-        # noise_mean=(0.0, 0.0, 0.0),  # Optional: Mean of Gaussian noise for measurements
-        # noise_std=(0.01, 0.01, 0.01),  # Optional: Standard deviation of noise
-        # gravity_compensation=True,  # Optional: Compensate for gravity in accelerometer
-        # gravity_bias=(0.0, 0.0, 0.0),  # Optional: Bias in gravity measurement
-        # offset=ImuCfg.OffsetCfg(  # Optional: Sensor mounting offset
-        #     pos=(0.0, 0.0, 0.0),
-        #     rot=(1.0, 0.0, 0.0, 0.0),  # Quaternion (w,x,y,z)
-        #     convention="ros"  # Coordinate convention (ros or isaac)
+# TODO: finish V2 model and configuration, not tested yet!
+# Model2, slew, lift, tilt, scoop, gripper rotator, gripper
+MASIV2_CFG = ArticulationCfg(
+    prim_path="{ENV_REGEX_NS}/Robot",
+    spawn=sim_utils.UsdFileCfg(
+        # Hardcoded path to the .usd file!
+        usd_path="C:/Users/sh23937/Documents/GitHub/IsaacSim/IsaacSim Models/Test_Bench_Model/for_scripts/model2.usd",
+
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            # Required properties
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=5.0,  # Increased from 1.0 to help with collision resolution (like Franka)
+
+            # Additional properties from examples
+            enable_gyroscopic_forces=True,
+            max_contact_impulse=1e4,  # Maximum contact force allowed (limit impacts)
+        ),
+
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,  # Keep false for efficiency if no self-collisions expected
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=0,
+            sleep_threshold=0.005,
+            stabilization_threshold=0.001,
+
+        ),
+
+
+        collision_props=sim_utils.CollisionPropertiesCfg(
+            contact_offset=0.002,  # Distance to start collision detection
+            rest_offset=0.0,  # Rest distance between colliding objects
+        ),
+        copy_from_source=True,
     ),
-    "imu_tilt": ImuCfg(
-        prim_path="{ENV_REGEX_NS}/Robot/imu_tiltboom",
-        update_period=0.0,
-        debug_vis=True,
-        # ...
+
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.0),
+
+        # initial joint angles in radians
+        joint_pos={
+            "revolute_cabin": 0.0,
+            "revolute_lift": 0.0,
+            "revolute_tilt": 0.0,
+            "revolute_scoop": 0.0,
+            "revolute_gripper": 0.0,
+            "revolute_claw": 0.0, # goal is to use mimic joint, so only one joint command for open/close needed
+        },
+        joint_vel={".*": 0.0},  # Set all joint velocities to zero initially
     ),
-    "imu_scoop": ImuCfg(
-        prim_path="{ENV_REGEX_NS}/Robot/imu_scoop",
-        update_period=0.0,
-        debug_vis=True,
-    ),
-}
-"""
+
+    # Soft joint limits to prevent hitting hard stops
+    soft_joint_pos_limit_factor=1.00,
+
+    actuators={
+        "excavator": ImplicitActuatorCfg(
+            joint_names_expr=["revolute_cabin", "revolute_lift", "revolute_tilt", "revolute_scoop", "revolute_gripper", "revolute_claw"],
+
+            # Actuator configs directly from the USD file
+        ),
+    },
+)
